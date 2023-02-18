ed2c12a084baaae93145707ac492884600df5173
Mnilsson183 <morgan.nilsson7111@icloud.com>
2023-02-18 11:47:45 -0700

    commit

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,17 @@ use std::io;
 
 fn main() {
     println!("what units do you want to convert");
-    println!("X -> mX");
+    println!("Format in [X -> mX]");
+
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    loop
+    {
+        println!("Hello");
+        break;
+    }
 }

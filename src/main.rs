7afd9de102353cd0144faf096738bc4e8ae24e31
Mnilsson183 <morgan.nilsson7111@icloud.com>
2023-02-18 12:24:46 -0700

    commit

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::io;
 
 fn main() {
     println!("what units do you want to convert");
-    println!("Format in [X -> mX]");
+    println!("Format in [X Units->Final Unit");
 
     let mut input = String::new();
 
@@ -10,9 +10,21 @@ fn main() {
         .read_line(&mut input)
         .expect("Failed to read line");
 
-    loop
+    let middle: u32 = 0;
+
+    let input_vector: Vec<char> = input.chars().collect();
+
+    for middle in 0..input_vector.len()-1
     {
-        println!("Hello");
-        break;
+        if input_vector[middle] == '-' && input_vector[middle + 1] == '>'
+        {
+            println!("{}", middle);
+            break;
+        }
     }
+
+    let pre = vec!['T', 'G', 'M', 'k', 'm', 'μ', 'n', 'p'];
+    let pre_value = vec![12, 9, 6, 3, -3, -6, -9, -12];
+
+
 }

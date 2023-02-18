177485a195669fc3ca81d5708cf6ef202c3f0dfa
Mnilsson183 <morgan.nilsson7111@icloud.com>
2023-02-18 12:47:54 -0700

    bad project

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::io;
 
 fn main() {
     println!("what units do you want to convert");
-    println!("Format in [X Units->Final Unit");
+    println!("Format in [X Units->Final Unit]");
 
     let mut input = String::new();
 
@@ -10,7 +10,7 @@ fn main() {
         .read_line(&mut input)
         .expect("Failed to read line");
 
-    let middle: u32 = 0;
+    let middle: usize = 0;
 
     let input_vector: Vec<char> = input.chars().collect();
 
@@ -26,5 +26,39 @@ fn main() {
     let pre = vec!['T', 'G', 'M', 'k', 'm', 'μ', 'n', 'p'];
     let pre_value = vec![12, 9, 6, 3, -3, -6, -9, -12];
 
+    let mut input_units:usize = 0;
+    let mut input_value:usize = 0;
+    let mut output_units:usize = 0;
+    let mut output_value:usize = 0;
 
+    for i in (0..pre.len())
+    {
+        if input_vector[0] == pre[i]
+        {
+            input_units = i;
+        }
+        else
+        {
+            if input_vector[0] == pre[pre.len() - 1]
+            {
+                println!("The first unit value is either not supported or not a unit");
+            }
+        }
+    }
+    for i in (0..pre.len())
+    {
+        if input_vector[middle + 2] == pre[i]
+        {
+            output_units = i;
+        }
+        else
+        {
+            if input_vector[0] == pre[pre.len() -  1]
+            {
+                println!("The second unit value is either not supported or not a unit");
+            }
+        }
+    }
+    println!("{input_units}");
+    println!("{output_units}");
 }
